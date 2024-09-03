df88c1e3ddf8359b9eed16cbf282c0404622c753
Cale Smith <calepayson@mac.com>
2024-09-03 14:54:51 -0700

    Handle forecast data requests

--- surfglass/requests.py
+++ surfglass/requests.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from typing import List
 import arrow
 import os
 import requests
@@ -15,6 +16,23 @@ def fetch_tide_data(latitude: float, longitude: float) -> dict:
             'lat': latitude ,
             'lng': longitude,
             'start': start.to('PST').timestamp(), # Convert to PST timestamp
+        },
+        headers={
+            'Authorization': get_api_key()
+        }
+    )
+    response.raise_for_status()
+    return response.json()
+
+def fetch_forecast_data(latitude: float, longitude: float, requested_data_points: List[str]) -> dict:
+    start = arrow.now().floor('day')
+    response = requests.get(
+        'https://api.stormglass.io/v2/weather/point',
+        params={
+            'lat': latitude,
+            'lng': longitude,
+            'params': ','.join(requested_data_points),
+            'start': start.to('PST').timestamp(),
         },
         headers={
             'Authorization': get_api_key()
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,13 +1,13 @@
 import pytest
 import requests_mock
-from surfglass.coordinates import Coordinates
-from surfglass.requests import get_api_key, fetch_tide_data
+from surfglass.requests import get_api_key, fetch_tide_data, fetch_forecast_data
 
 def test_get_api_key():
     """Test that the API key was properly imported"""
     assert get_api_key != None
 
 def test_fetch_tide_data():
+    """Test that a proper request is used for tide data"""
     with requests_mock.Mocker() as m:
         m.get(
             'https://api.stormglass.io/v2/tide/sea-level/point',
@@ -22,3 +22,21 @@ def test_fetch_tide_data():
         assert response['status'] == 'success'
         assert response['data'] == 'mocked data'
         assert 'lat=60.936&lng=-42.69' in m.last_request.query
+
+def test_fetch_forecast_data():
+    """Test that a proper request is used for forecast data"""
+    with requests_mock.Mocker() as m:
+        m.get(
+            'https://api.stormglass.io/v2/weather/point',
+            json={
+                'status': 'success',
+                'data': 'mocked data',
+            }
+        )
+
+        response = fetch_forecast_data(60.936, -42.69,["weather", "current", "swell"])
+        print(m.last_request.query)
+
+        assert response['status'] == 'success'
+        assert response['data'] == 'mocked data'
+        assert 'lat=60.936&lng=-42.69&params=weather%2ccurrent%2cswell' in m.last_request.query
