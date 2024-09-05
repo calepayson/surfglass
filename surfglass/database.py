ce8b066c735079efe7a4df38e2a2d1db70caa805
Cale Smith <calepayson@mac.com>
2024-09-05 10:41:03 -0700

    Refactor database.py

--- surfglass/database.py
+++ surfglass/database.py
@@ -1,82 +1,70 @@
 import sqlite3
-from sqlite3 import Error
+
+CREATE_LOCATIONS_TABLE = """
+CREATE TABLE IF NOT EXISTS locations (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL
+);
+"""
+
+CREATE_UPDATES_TABLE = """
+CREATE TABLE IF NOT EXISTS updates (
+        id INTEGER PRIMARY KEY,
+        time TEXT NOT NULL
+        );
+"""
+
+CREATE_FORECASTS_TABLE = """
+CREATE TABLE IF NOT EXISTS forecasts (
+    id INTEGER PRIMARY KEY,
+    location_id INTEGER NOT NULL,
+    time TEXT NOT NULL,
+    tide REAL,
+    air_temp REAL,
+    cloud_cover REAL,
+    current_direction REAL,
+    current_speed REAL,
+    gust REAL,
+    swell_direction REAL,
+    swell_height REAL,
+    swell_period REAL,
+    secondary_swell_direction REAL,
+    secondary_swell_height REAL,
+    secondary_swell_period REAL,
+    visibility REAL,
+    wave_direction REAL,
+    wave_height REAL,
+    wave_period REAL,
+    wind_wave_direction REAL,
+    wind_wave_height REAL,
+    wind_wave_period REAL,
+    wind_direction REAL,
+    wind_direction1000hpa REAL,
+    wind_speed REAL,
+    wind_speed1000hpa REAL,
+    FOREIGN KEY (location_id) REFERENCES locations (id)
+);
+"""
 
 def create_connection(db_file):
     """Create a database connection to SQLite database specified by db_file"""
-    connection = None
-    try:
-        connection = sqlite3.connect(db_file)
-        connection.execute("PRAGMA foreign_keys = ON")
-        print(f"Connected to SQLite database: {db_file}")
-    except Error as e:
-        print(e)
+    connection = sqlite3.connect(db_file)
+    connection.execute("PRAGMA foreign_keys = ON")
     return connection
 
 def create_locations_table(connection):
     """Create the locations table in the database with the supplied connection"""
-    try:
-        create_locations_table_sql = """
-        CREATE TABLE IF NOT EXISTS locations (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            latitude REAL NOT NULL,
-            longitude REAL NOT NULL
-        );
-        """
-        cursor = connection.cursor()
-        cursor.execute(create_locations_table_sql)
-    except Error as e:
-        print(e)
-
-def create_forecasts_table(connection):
-    """Create the forecasts table in the database with the supplied connection"""
-    try:
-        create_forecasts_table_sql = """
-        CREATE TABLE IF NOT EXISTS forecasts (
-            id INTEGER PRIMARY KEY,
-            location_id INTEGER NOT NULL,
-            time TEXT NOT NULL,
-            tide REAL,
-            air_temp REAL,
-            cloud_cover REAL,
-            current_direction REAL,
-            current_speed REAL,
-            gust REAL,
-            swell_direction REAL,
-            swell_height REAL,
-            swell_period REAL,
-            secondary_swell_direction REAL,
-            secondary_swell_height REAL,
-            secondary_swell_period REAL,
-            visibility REAL,
-            wave_direction REAL,
-            wave_height REAL,
-            wave_period REAL,
-            wind_wave_direction REAL,
-            wind_wave_height REAL,
-            wind_wave_period REAL,
-            wind_direction REAL,
-            wind_direction1000hpa REAL,
-            wind_speed REAL,
-            wind_speed1000hpa REAL,
-            FOREIGN KEY (location_id) REFERENCES locations (id)
-        );
-        """
-        cursor = connection.cursor()
-        cursor.execute(create_forecasts_table_sql)
-    except Error as e:
-        print(e)
+    with connection:
+        connection.execute(CREATE_LOCATIONS_TABLE)
 
 def create_updates_table(connection):
     """Create the updates table in the database with the supplied connection"""
-    try:
-        create_updates_table_sql = """
-        CREATE TABLE IF NOT EXISTS updates (
-            id INTEGER PRIMARY KEY,
-            time TEXT NOT NULL
-        );
-        """
-        cursor = connection.cursor()
-        cursor.execute(create_updates_table_sql)
-    except Error as e:
-        print(e)
+    with connection:
+        connection.execute(CREATE_UPDATES_TABLE)
+
+def create_forecasts_table(connection):
+    """Create the forecasts table in the database with the supplied connection"""
+    with connection:
+        connection.execute(CREATE_FORECASTS_TABLE)
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,23 @@
 import pytest
 import sqlite3
-from surfglass.database import create_connection, create_locations_table, create_updates_table, create_forecasts_table
+import surfglass.database as Database
+
+LOCATIONS_EXPECTED_COLUMNS = ['id', 'name', 'latitude', 'longitude']
+UPDATES_EXPECTED_COLUMNS = ['id', 'time']
+FORECASTS_EXPECTED_COLUMNS = [
+    'id', 'location_id', 'time', 'tide', 'air_temp', 'cloud_cover', 
+    'current_direction', 'current_speed', 'gust', 'swell_direction', 
+    'swell_height', 'swell_period', 'secondary_swell_direction', 
+    'secondary_swell_height', 'secondary_swell_period', 'visibility', 
+    'wave_direction', 'wave_height', 'wave_period', 'wind_wave_direction', 
+    'wind_wave_height', 'wind_wave_period', 'wind_direction', 
+    'wind_direction1000hpa', 'wind_speed', 'wind_speed1000hpa'
+]
 
 def test_create_connection(tmp_path):
     """Test that create connection successfully establishes a connection to the database"""
     db_file = tmp_path / "test.db"
-    connection = create_connection(str(db_file))
+    connection = Database.create_connection(str(db_file))
     assert connection is not None, "create_connection() returned 'None'"
     assert connection.execute("PRAGMA foreign_keys").fetchone()[0] == 1, "Foreign keys not initialized"
     connection.close()
@@ -13,8 +25,8 @@ def test_create_connection(tmp_path):
 def test_create_locations_table(tmp_path):
     """Test that the locations table is successfully created"""
     db_file = tmp_path / "test.db"
-    connection = create_connection(str(db_file))
-    create_locations_table(connection)
+    connection = Database.create_connection(str(db_file))
+    Database.create_locations_table(connection)
 
     # Check if the table exists
     result = connection.execute("""
@@ -27,47 +39,15 @@ def test_create_locations_table(tmp_path):
     # Check if the table has the right schema
     result = connection.execute("PRAGMA table_info(locations);")
     columns = [row[1] for row in result.fetchall()]
-    expected_columns = ['id', 'name', 'latitude', 'longitude']
-    for column in expected_columns:
+    for column in LOCATIONS_EXPECTED_COLUMNS:
         assert column in columns, f"The locations table is missing a column: {column}"
     connection.close()
-
-def test_create_forecasts_table(tmp_path):
-    """Test that the forecasts table is successfully created"""
-    db_file = tmp_path / "test.db"
-    connection = create_connection(str(db_file))
-    create_locations_table(connection)
-    create_forecasts_table(connection)
-
-    # Check if the table exists
-    result = connection.execute("""
-        SELECT name
-        FROM sqlite_master
-        WHERE type='table' AND name='forecasts';
-    """)
-    assert result.fetchone is not None, "forecasts table not found"
-
-    # Check if the table has the right schema
-    result = connection.execute("PRAGMA table_info(forecasts);")
-    columns = [row[1] for row in result.fetchall()]
-    expected_columns = [
-        'id', 'location_id', 'time', 'tide', 'air_temp', 'cloud_cover',
-        'current_direction', 'current_speed', 'gust', 'swell_direction',
-        'swell_height', 'swell_period', 'secondary_swell_direction',
-        'secondary_swell_height', 'secondary_swell_period', 'visibility',
-        'wave_direction', 'wave_height', 'wave_period', 'wind_wave_direction',
-        'wind_wave_height', 'wind_wave_period', 'wind_direction',
-        'wind_direction1000hpa', 'wind_speed', 'wind_speed1000hpa'
-    ]
-    for column in expected_columns:
-        assert column in columns, f"The forecasts table is missing a column: {column}"
-
 
 def test_create_updates_table(tmp_path):
     """Test that the updates table is successfully created"""
     db_file = tmp_path / "test.db"
-    connection = create_connection(str(db_file))
-    create_updates_table(connection)
+    connection = Database.create_connection(str(db_file))
+    Database.create_updates_table(connection)
 
     # Check if the table exists
     result = connection.execute("""
@@ -80,7 +60,28 @@ def test_create_updates_table(tmp_path):
     # Check if the table has the right schema
     result = connection.execute("PRAGMA table_info(updates);")
     columns = [row[1] for row in result.fetchall()]
-    expected_columns = ['id', 'time']
-    for column in expected_columns:
+    for column in UPDATES_EXPECTED_COLUMNS:
         assert column in columns, f"The updates table is missing a column: {column}"
     connection.close()
+
+def test_create_forecasts_table(tmp_path):
+    """Test that the forecasts table is successfully created"""
+    db_file = tmp_path / "test.db"
+    connection = Database.create_connection(str(db_file))
+    Database.create_locations_table(connection)
+    Database.create_forecasts_table(connection)
+
+    # Check if the table exists
+    result = connection.execute("""
+        SELECT name
+        FROM sqlite_master
+        WHERE type='table' AND name='forecasts';
+    """)
+    assert result.fetchone is not None, "forecasts table not found"
+
+    # Check if the table has the right schema
+    result = connection.execute("PRAGMA table_info(forecasts);")
+    columns = [row[1] for row in result.fetchall()]
+    for column in FORECASTS_EXPECTED_COLUMNS:
+        assert column in columns, f"The forecasts table is missing a column: {column}"
+
